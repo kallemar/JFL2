201c96dfec06c03ecf9bd0cffa1739184d620c03
Kalle Marjamäki <kalle@vilkas.fi>
2018-02-06 16:12:01 +0200

    testig

--- lib/test.pl
+++ lib/test.pl
@@ -6,22 +6,64 @@ use XML::XPath;
 
 
 
- # Create XML for post   
+#  Create XML for post   
 my $RootNode = XML::XPath::Node::Element->new('root',"");
-my $customer_xml = XML::XPath->new(context => $RootNode);
-
-$customer_xml->createNode("/customer/customerbaseinformation/internalidentifier");
-$customer_xml->setNodeText("/customer/customerbaseinformation/internalidentifier", '123456');
-my $text = $customer_xml->getNodeText("/customer/customerbaseinformation/internalidentifier");
-print $text . "\n";
+my $invoice_xml = XML::XPath->new(context => $RootNode);
 
 
-my $AttributeNode = XML::XPath::Node::Attribute->new("TYPE", "KUKKUU", '');
-my $Node = $customer_xml->find("/customer/customerbaseinformation/internalidentifier")->get_node(1);
-$Node->appendAttribute($AttributeNode);
+  
+# INVOICE LINE 1
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productidentifier", 'netvisorid');
+_xmlSetAttribute($invoice_xml, 	"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productidentifier", "type", "netvisor");
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productname", 'name');
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productunitprice", 'price');
+_xmlSetAttribute($invoice_xml, 	"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productunitprice", "type", "net");
+
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productvatpercentage", "0");
+_xmlSetAttribute($invoice_xml, 	"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productvatpercentage", "vatcode", "KOMY");
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/salesinvoiceproductlinequantity", "1");
+	
+# SET INVOICE LINE 2
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productidentifier[last()]", 'Netvisor_TShirtDiscountProductID');
+_xmlSetAttribute($invoice_xml, 	"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productidentifier[last()]", "type", "netvisor");
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productname[last()]", 'name');
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productunitprice[last()]", 'price');
+_xmlSetAttribute($invoice_xml, 	"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productunitprice[last()]", "type", "net");
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productvatpercentage[last()]", "0");
+_xmlSetAttribute($invoice_xml, 	"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/productvatpercentage[last()]", "vatcode", "KOMY");
+_xmlset($invoice_xml, 			"/salesinvoice/invoicelines/invoiceline/salesinvoiceproductline/salesinvoiceproductlinequantity[last()]", "1");
 
 
-my $data = $customer_xml->findnodes_as_string('/');
+
+
+my $data = $invoice_xml->findnodes_as_string('/');
 print $data . "\n";    
 
 
+sub _xmlset {
+    my ($Tree, $xpath, $value) = @_;
+
+    if (!$Tree->exists($xpath)) {
+        $Tree->createNode($xpath);
+    
+        if((defined $value) and ($value ne '')) {
+        $Tree->setNodeText($xpath, $value);
+        }
+    }
+}
+
+
+sub _xmlSetAttribute {
+    my $Tree = shift;
+    my $xpath = shift;
+    my $AttributeKey = shift;
+    my $AttributeValue = shift;
+
+    if(!$Tree->exists($xpath)) {
+        $Tree->createNode($xpath);
+    }
+
+    my $Node = $Tree->find($xpath)->get_node(1);
+    my $AttributeNode = XML::XPath::Node::Attribute->new($AttributeKey, $AttributeValue, '');
+    $Node->appendAttribute($AttributeNode);
+}
